7c2c0785ac80977b1fde42c2146560ae01365a48
JohnnyLU <ctygholog304@gmail.com>
2026-06-02 22:34:15 +0500

    feature: add validate middleware

---
+++ backend/src/middlewares/validate.middleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodSchema } from "zod";
+import { AppError } from "./error.middleware.js";
+
+export function validate(schema: ZodSchema) {
+  return (request: Request, response: Response, next: NextFunction) => {
+    const result = schema.safeParse(request.body);
+
+    if (!result.success) {
+      const message = result.error.issues[0]?.message || "Validation error";
+      return next(new AppError(message, 400));
+    }
+
+    request.body = result.data;
+    next();
+  }
+}
