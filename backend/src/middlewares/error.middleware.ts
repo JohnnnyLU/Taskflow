18e76f9e5a09e690574ffdc8b493e0aa5180247e
JohnnyLU <ctygholog304@gmail.com>
2026-06-03 17:27:18 +0500

    fix: update error

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 export class AppError extends Error {
   statusCode: number;
 
-  constructor(message: string, statusCode: 500) {
+  constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
   }
