0f0db09165a41acdf46228e7396a3b88998a7fac
JohnnyLU <ctygholog304@gmail.com>
2026-06-03 21:29:55 +0500

    feat: add auth routes

---
+++ backend/src/modules/auth/auth.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { validate } from "@/middlewares/validate.middleware.js";
+import { authMiddleware } from "@/middlewares/auth.middleware.js";
+import { loginSchema, registerSchema } from "./auth.schemas.js";
+import {
+  loginController,
+  meController,
+  registerController
+} from "./auth.controller.js";
+
+export const authRoutes = Router();
+
+authRoutes.post("/register", validate(registerSchema), registerController);
+authRoutes.post("/login", validate(loginSchema), loginController);
+authRoutes.get("/me", authMiddleware, meController);
