3d6da3b2320b67ecec47b706fa638db3bfefc803
JohnnyLU <ctygholog304@gmail.com>
2026-06-04 15:29:40 +0500

    feat: add prettier format

---
+++ frontend/prettier.config.js
@@ -0,0 +1,13 @@
+export default {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 90,
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindStylesheet: './src/app/globals.css'
+};
