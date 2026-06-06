9880c3b70396466e2d7917e3e42af78b05d0b42f
JohnnyLU <ctygholog304@gmail.com>
2026-06-06 23:46:14 +0500

    feat: add tasks routes

---
+++ backend/src/modules/tasks/tasks.routes.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { authMiddleware } from "@/middlewares/auth.middleware.js";
+import { validate } from "@/middlewares/validate.middleware.js";
+import {
+  createTaskSchema,
+  updateTaskStatusSchema,
+} from "@/modules/tasks/tasks.schemas.js";
+import {
+  createTaskController,
+  deleteTaskController,
+  getTasksController,
+  updateTaskStatusController,
+} from "@/modules/tasks/tasks.controller.js";
+
+export const tasksRoutes = Router();
+
+tasksRoutes.use(authMiddleware);
+
+tasksRoutes.get("/projects/:projectId/tasks", getTasksController);
+tasksRoutes.post(
+  "/projects/:projectId/tasks",
+  validate(createTaskSchema),
+  createTaskController
+);
+tasksRoutes.patch(
+  "/tasks/:taskId/status",
+  validate(updateTaskStatusSchema),
+  updateTaskStatusController
+);
+tasksRoutes.delete("/tasks/:taskId", deleteTaskController);
